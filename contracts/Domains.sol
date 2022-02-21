f45f8425d28301743dfb8870d2a6eaf413b59f9e
Gerhard Molin <gerhard@minimumbadass.com>
2022-02-21 15:17:31 +0100

    added register and domain owner address function

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,7 +5,27 @@ import "hardhat/console.sol";
 
 contract Domains {
 
+	/*
+		a map/hash table data type for storing the domain names
+	*/
+	mapping(string => address) public domains;
+
 	constructor() {
 		console.log('This is my domain contract');
+	}
+
+	/*
+		A register function for registering a domain, to our mapping domains
+	*/
+	function register(string calldata _newDomainName) public {
+		domains[_newDomainName] = msg.sender;
+		console.log("%s has registered a domain!", msg.sender);
+	}
+
+	/*
+		This function will return the owner of a domain
+	*/
+	function getDomainOwnerAddress(string calldata _domainName) public view returns (address) {
+		return domains[_domainName];
 	}
 }
