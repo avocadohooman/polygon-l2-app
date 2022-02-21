a5dfeca9214eff74507664115bcacc40a3e4a041
Gerhard Molin <gerhard@minimumbadass.com>
2022-02-21 15:57:51 +0100

    added priceOfDomain function

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,10 +1,15 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.10;
 
+// Don't forget to add this import
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
 
+	// Here's our domain TLD!
+	string public tld;
+	
 	/*
 		a map/hash table data type for storing the domain names
 	*/
@@ -15,8 +20,25 @@ contract Domains {
 	*/
 	mapping(string => string) public records;
 
-	constructor() {
-		console.log('This is my domain contract');
+	constructor(string memory _tld) payable {
+		tld = _tld;
+		console.log("%s name service deployed", _tld);
+	}
+
+	/*
+		This functions gives us the pice of a domain name, based
+		on the amount of characters
+	*/
+	function priceOfDomain(string calldata _newDomainName) public pure returns (uint) {
+		uint len = StringUtils.strlen(_newDomainName);
+		require(len > 0);
+		if (len == 3) {
+			return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+		} else if (len == 4) {
+			return 3 * 10**17; // 0.3 MATIC
+		} else {
+			return 1 * 10**17; // 0.1 MATIC
+		}
 	}
 
 	/*
@@ -24,8 +46,14 @@ contract Domains {
 		Calldata is non-persistent and can’t be modified. We like this because 
 		it takes the least amount of gas!
 	*/
-	function register(string calldata _newDomainName) public {
-		domains[_newDomainName] = msg.sender;
+	function register(string calldata _newDomainName) public payable{
+		// Check that the name is unregistered
+		require(domains[_newDomainName] == address(0));
+
+		uint _price = priceOfDomain(_newDomainName);
+
+		require(msg.value >= _price, "Not enough matic paid");
+				domains[_newDomainName] = msg.sender;
 		console.log("%s has registered a domain!", msg.sender);
 	}
 
--- scripts/run.js
+++ scripts/run.js
@@ -1,16 +1,19 @@
 const main = async () => {
 	const [owner, randomPerson] = await hre.ethers.getSigners();
 	const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-	const domainContract = await domainContractFactory.deploy();
+	const domainContract = await domainContractFactory.deploy("avocado");
 	await domainContract.deployed();
 	console.log('Deployed contract to:', domainContract.address);
 	console.log('Deployed by:', owner.address);
 
-	const txn = await domainContract.register("doom");
+	let txn = await domainContract.register("mortal",  {value: hre.ethers.utils.parseEther('0.1')});
 	await txn.wait();
 
-	const domainOwner = await domainContract.getDomainOwnerAddress("doom");
+	const domainOwner = await domainContract.getDomainOwnerAddress("mortal");
 	console.log("Owner of domain:", domainOwner);
+
+	const balance = await hre.ethers.provider.getBalance(domainContract.address);
+	console.log("Contract balance:", hre.ethers.utils.formatEther(balance));  
 }
 
 const runMain = async () => {
