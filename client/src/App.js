17695892573554f948a97f3c5598518fc0911bce
Gerhard Molin <gerhard@minimumbadass.com>
2022-02-24 14:49:33 +0100

    adding connect button

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,91 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 
 // Constants
 
 const App = () => {
 
-  return (
-		<div className="App">
-			<div className="container">
+	//Just a state variable we use to store our user's public wallet. Don't forget to import useState at the top.
+	const [currentAccount, setCurrentAccount] = useState('');
 
-				<div className="header-container">
-					<header>
-						<div className="left">
-							<p className="title">🐱‍👤 Avocado Name Service</p>
-							<p className="subtitle">Your immortal API on the blockchain!</p>
-						</div>
-					</header>
-				</div>
+	// Implement your connectWallet method here
+	const connectWallet = async () => {
+		try {
+			const { ethereum } = window;
 
-			</div>
+			if (!ethereum) {
+				alert("Get MetaMask -> https://metamask.io/");
+				return;
+			}
+
+			// Fancy method to request access to account.
+			const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+		
+			// Boom! This should print out public address once we authorize Metamask.
+			console.log("Connected", accounts[0]);
+			setCurrentAccount(accounts[0]);
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	const checkIfWalletIsConnected = async () => {
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			console.log('Make sure you have metamask!');
+			return;
+		} else {
+			console.log('We have the ethereum object', ethereum);
+		}
+
+		// Check if we're authorized to access the user's wallet
+		const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+		// Users can have multiple authorized accounts, we grab the first one if its there!
+		if (accounts.length !== 0) {
+			const account = accounts[0];
+			console.log('Found an authorized account:', account);
+			setCurrentAccount(account);
+		} else {
+			console.log('No authorized account found');
+		}
+	};
+
+	// Create a function to render if wallet is not connected yet
+	const renderNotConnectedContainer = () => (
+		<div className="connect-wallet-container">
+			<img src="https://media.giphy.com/media/3ohhwytHcusSCXXOUg/giphy.gif" alt="Ninja gif" />
+			{/* Call the connectWallet function we just wrote when the button is clicked */}
+			<button onClick={connectWallet} className="cta-button connect-wallet-button">
+				Connect Wallet
+			</button>
 		</div>
+	);
+
+	// This runs our function when the page loads.
+	useEffect(() => {
+		checkIfWalletIsConnected();
+	}, [])
+
+	return (
+			<div className="App">
+				<div className="container">
+
+					<div className="header-container">
+						<header>
+							<div className="left">
+								<p className="title">🐱‍👤 Avocado Name Service</p>
+								<p className="subtitle">Your immortal API on the blockchain!</p>
+							</div>
+						</header>
+					</div>
+
+					{/* Hide the connect button if currentAccount isn't empty*/}
+					{currentAccount && renderNotConnectedContainer()}
+
+				</div>
+			</div>
 	);
 }
 
